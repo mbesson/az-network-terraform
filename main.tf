16173a6b52e19660387ca4494ccc6e26a0ddad6e
Mathieu Besson <mathieu@iedublde78vb9k3.clarinsgroup.com>
2022-08-20 20:26:07 +0200

    [MBE]: Add tagging

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "rg" {
   name      = var.resource_group_name
   location  = var.resource_group_location
+
+  tags = {
+    "environment" = "UAT"
+    "project" = "Terraform Network"
+  }
 }
