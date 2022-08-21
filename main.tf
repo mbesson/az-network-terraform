f58269a5764e8cf7bce93bec9537dfb77181d953
Mathieu Besson <mathieu@iedublde78vb9k3.clarinsgroup.com>
2022-08-21 10:36:24 +0200

    [MBE]: Added network configuration

--- main.tf
+++ main.tf
@@ -1,9 +1,95 @@
-resource "azurerm_resource_group" "rg" {
-  name      = var.resource_group_name
+# Creating ressource group for main ressources of the project
+resource "azurerm_resource_group" "main-rg" {
+  name      = "lo3-we-lovelace-rg-001"
+  location  = var.resource_group_location
+
+  tags = {
+    "environment" = "UAT"
+    "project" = "Lovelace"
+    "module" = "Main"
+  }
+}
+
+# Creating ressource group for Databricks nodes
+resource "azurerm_resource_group" "databricks-managed-rg" {
+  name      = "lo3-we-lovelace-rg-002"
+  location  = var.resource_group_location
+
+  tags = {
+    "environment" = "UAT"
+    "project" = "Lovelace"
+    "module" = "Databricks"
+  }
+}
+
+# Creating ressource group for network resources
+resource "azurerm_resource_group" "network-rg" {
+  name      = "lo3-we-network-rg-001"
   location  = var.resource_group_location
 
   tags = {
     "environment" = "UAT"
     "project" = "Terraform Network"
+    "module" = "Network"
+  }
+}
+
+# Creating network ressources
+
+# The main vnet
+resource "azurerm_virtual_network" "main-vnet" {
+  name                = "lo3-we-lovelace-vnet-001"
+  address_space       = ["10.230.5.0/24"]
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.network-rg.name
+}
+
+# The main subnet
+resource "azurerm_subnet" "main-subnet" {
+  name                 = "lo3-we-lovelace-sub-001"
+  resource_group_name  = azurerm_resource_group.network-rg.name
+  virtual_network_name = azurerm_virtual_network.main-vnet.name
+  address_prefixes     = ["10.230.5.0/25"]
+}
+
+# The public subnet for Databricks
+resource "azurerm_subnet" "pub-subnet" {
+  name                 = "lo3-we-lovelace-sub-pub-001"
+  resource_group_name  = azurerm_resource_group.network-rg.name
+  virtual_network_name = azurerm_virtual_network.main-vnet.name
+  address_prefixes     = ["10.230.5.128/26"]
+
+  delegation {
+    name = "databricks-delegation"
+
+    service_delegation {
+      name = "Microsoft.Databricks/workspaces"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
+      ]
+    }
+  }
+}
+
+# The private subnet for Databricls
+resource "azurerm_subnet" "prv-subnet" {
+  name                 = "lo3-we-lovelace-sub-prv-001"
+  resource_group_name  = azurerm_resource_group.network-rg.name
+  virtual_network_name = azurerm_virtual_network.main-vnet.name
+  address_prefixes     = ["10.230.5.192/26"]
+
+  delegation {
+    name = "databricks-delegation"
+
+    service_delegation {
+      name = "Microsoft.Databricks/workspaces"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
+      ]
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -27,11 +27,11 @@ variable "resource_group_name_prefix" {
   description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable "resource_group_name" {
-  type     = string
-  nullable = false
-  description = "The name of the ressource groups you want to create."
-}
+#variable "resource_group_name" {
+#  type     = string
+#  nullable = false
+#  description = "The name of the ressource groups you want to create."
+#}
 
 variable "resource_group_location" {
   default = "westeurope"
